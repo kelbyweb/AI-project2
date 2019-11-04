7c2ccfcd230256581ecfdb451c56c79ea63c8dd1
Abel Shinabery <abelpshinabery@gmail.com>
2019-11-04 12:20:30 -0600

    Add files via upload

---
+++ Towers.h
@@ -0,0 +1,18 @@
+#include <vector>
+#include <deque>
+
+enum TowerName {A, B, C};
+
+class HanoiTower {
+private:
+    std::deque <int> disks;
+    int total;
+public:
+    HanoiTower();
+    int getTotal();
+    int getTopDisk();
+    std::deque <int> getDisks();
+    void setDisks(std::deque <int> &disks);
+    int popTopDisk();
+    void pushDisk(int i);
+};
